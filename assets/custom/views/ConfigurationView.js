f37ecb3267375186e07e656c63b7c9e298d4cfd1
Jose Manuel Perez <jperez@ONLINE-JOSE-MANUEL-PEREZ.local>
2015-08-19 15:29:40 -0500

    Se sube menú configuración

--- assets/custom/views/ConfigurationView.js
+++ assets/custom/views/ConfigurationView.js
@@ -1,17 +1,56 @@
 var ConfigurationView = MasterView.extend({
 	tagName : 'ul',
-	className : 'sidebar-view',
+	className : 'sidebar-view configuration animated slideOutRight',
 	templateURL : YaGlobals.CONFIG_MENU_TEMPLATE,
+	events : {
+		'click .close' : 'hide'
+	},
 	initialize : function(){
+		_.bindAll(this,'hide');
+		this.$el.hide();
 		this.constructor.__super__.initialize.apply(this, []);
 	},
 	
 	render : function(){
 		this.$el.append(this.template());
+		this.trigger('onViewRendered',this);
+		return this;
 	},
+	
+//	animationEnd : function(event){
+//		this.$el.removeClass('slideOutRight');
+//		this.$el.hide();
+//		this.trigger('onConfigHide');
+//		event.stopPropagation();
+//		this.$el.one('webkitAnimationEnd mozAnimationEnd '+
+//				  'MSAnimationEnd oanimationend animationend',this.animationEnd);
+//	},
 	
 	onViewReady : function(){
 		this.render();
-	}
+	},
+	
+	show : function(){
+		var _that = this;
+		this.$el.show();
+		this.$el.one('webkitAnimationEnd mozAnimationEnd '+
+				  'MSAnimationEnd oanimationend animationend',function(){
+			_that.$el.removeClass('slideInRight');
+		});
+		this.$el.removeClass('slideOutRight');
+		this.$el.addClass('slideInRight');
+	},
+	
+	hide : function(){
+		var _that = this;
+		this.$el.one('webkitAnimationEnd mozAnimationEnd '+
+				  'MSAnimationEnd oanimationend animationend',function(event){
+			_that.$el.removeClass('slideOutRight');
+			_that.$el.hide();
+			_that.trigger('onConfigHide');
+			event.stopPropagation();
+		});
+		this.$el.addClass('slideOutRight');
+	},
 	
 });
--- assets/custom/views/UserMenuView.js
+++ assets/custom/views/UserMenuView.js
@@ -10,21 +10,47 @@ var UserModel = Backbone.Model.extend({
 });
 
 var UserMenuView = MasterView.extend({
+	events : {
+		'click .mnuConfigAccount' : 'openConfigView'
+	},
 	tagName : 'ul',
-	className : 'sidebar-view',
+	className : 'sidebar-view animated ',
 	templateURL : YaGlobals.USER_MENU_TEMPLATE,
-	
+	configurationView : undefined,
+	WINDOW_EVENT : 'close',
 	initialize : function(){
+		_.bindAll(this,'openConfigView');
 		this.constructor.__super__.initialize.apply(this, []);
 	},
 	
 	render : function(){
 		this.$el.append(this.template(this.model.toJSON()));
+		this.configurationView = new ConfigurationView();
+		this.configurationView.on('onViewRendered',function(view){
+			this.$el.parent().append(view.$el);
+		},this);
+		this.configurationView.on('onConfigHide',this.backFromConfig,this);
 		this.trigger('onViewRendered',this);
 		return this;
 	},
 	
 	onViewReady : function(){
 		this.render();
-	}
+	},
+	
+	backFromConfig : function(){
+		this.$el.show();
+	},
+	
+	openConfigView : function(event) {
+		var _that = this;
+		this.$el.on('webkitAnimationEnd mozAnimationEnd '+
+				  'MSAnimationEnd oanimationend animationend',function(event){
+			_that.$el.removeClass('slideOutLeft');
+			_that.$el.hide();
+			event.stopPropagation();
+		});
+		this.configurationView.show();
+		this.$el.addClass('slideOutLeft');
+	},
 });
